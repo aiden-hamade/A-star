b479c48c81380a2c855db5fdd6110efc8ffa172d
BR0XICK <kessingerbrock@gmail.com>
2024-09-24 22:07:29 -0400

    added simple heuristic

--- a-star.py
+++ a-star.py
@@ -1,6 +1,25 @@
+from pyraminx import Pyraminx
+
 def a():
     return 'solution'
 
 
 def heuristic():
     return 'g()'
+
+#heuristic: max(number colors per side) - 1
+def ColorsPerSide(puzzle): 
+    colors_per_side = []
+    #iterate through each side
+    for side in range(puzzle.array.shape[1]):
+        colors = set()
+        for triangle in range(puzzle.array.shape[0]):
+            colors.add(puzzle.array[triangle][side])
+        colors_per_side.append(len(colors))
+
+    return max(colors_per_side) - 1
+        
+
+
+
+
