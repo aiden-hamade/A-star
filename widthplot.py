bcaeef7b995f784eab61ad6205b82738ee87b25d
BR0XICK <kessingerbrock@gmail.com>
2024-09-29 21:29:59 -0400

    fixed data

--- widthplot.py
+++ widthplot.py
@@ -5,7 +5,7 @@ widths = np.array([
     [12, 12, 12, 12, 12],
     [36, 24, 36, 24, 36],
     [132, 96, 96, 132, 84],
-    [504, 240, 458, 432, 372],
+    [504, 240, 348, 432, 372],
     [1932, 1620, 2328, 1656, 1584],
     [11196, 10500, 10824, 10368, 11796]
 ])
@@ -17,7 +17,7 @@ def main():
     plt.plot(k_values, average_widths)
 
     for i, (x, y) in enumerate(zip(k_values, average_widths)):
-        plt.text(x, y, f'({x},{y:.2f})', fontsize=9, ha='right')
+        plt.text(x, y, f'({x}, {y:.2f})', fontsize=9, ha='right')
 
     plt.xlabel('Number of moves applied to puzzle')
     plt.ylabel('Average number of nodes expanded in last iteration')
